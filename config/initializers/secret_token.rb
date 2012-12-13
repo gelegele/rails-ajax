10042f8985b5e79fd93c55b9236f7cb36e083df6
gelegele <Administrator@ishibashi-pc.cad.css.fujitsu.com>
2012-12-13 13:07:54 +0900

    rails new rails-ajax

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAjax::Application.config.secret_token = '922cc82fce49fe096bd5fc1a065705ccc59e05afd7831343aa864e28ef6f1f0493a791fd99564ffae2c93e7fd0058e9853d5468abb87dfff28e7b9afe5d1c584'
